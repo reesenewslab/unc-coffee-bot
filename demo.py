7ab42f0bf4beb7f3f69acad736133c4d775981f5
Bhargav <83196619+bhargavbompalli@users.noreply.github.com>
2024-03-27 12:59:05 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -12,7 +12,7 @@ arm.set_state(state=0) #Start with state 0 (ready)
 arm.set_gripper_mode(0) #Set the gripper mode to 0 (position control mode)
 
 
-#Here is an example of the arm moving to 3 positions, then opening, then closing the gripper
+#Here is an example of the arm moving to 3 positions sequentially, then opening, then closing the gripper
 #Change the x, y, z, roll, pitch, yaw, and speed based on your operation. wait=True will make the program wait until the arm finishes the movement before moving to the next line of code
 
 arm.set_position(x=378, y=254, z=355, roll=-180, pitch=0, yaw=-29.9, speed=100, is_radian=False, wait=True)
@@ -24,7 +24,7 @@ arm.set_position(x=250, y=254, z=355, roll=-180, pitch=0, yaw=-29.9, speed=100, 
 arm.set_gripper_position(850, wait=True) #This sets the gripper position to 850 (fully open)
 arm.set_gripper_position(0, wait=True) #This sets the gripper position to 0 (fully closed)
 
-#Here is an example of the arm moving to a position using joint angles, then opening and closing the gripper
+#Here is an example of the arm moving to 2 positions sequentially using joint angles, then opening and closing the gripper
 #Use this method if you want to control the arm using joint angles instead of x, y, z, roll, pitch, yaw
 #Change the angles and speed based on your operation. Each angle in the list corresponds to a joint angle from 1 to 6 (in degrees)
 
