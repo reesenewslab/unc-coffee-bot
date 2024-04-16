605669ad92cfbcb56de3d5b5300c8b681c157038
Caleb Sigmon <cjsigmon@email.unc.edu>
2024-04-15 20:52:20 -0400

    add coffee order endpoint

--- api/app.py
+++ api/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
-
-
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/order_coffee", methods=["POST"])
+def order_coffee():
+    data = request.json
+    coffee_type = data.get("coffee_type")
+    syrup_type = data.get("syrup_type")
+
+    if coffee_type:
+        drink_ordered = {"coffee_type": coffee_type}
+
+        if syrup_type:
+            drink_ordered["syrup_type"] = syrup_type
+
+        return jsonify(drink_ordered)
+    else:
+        return jsonify({"error": "Coffee type is required"}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
