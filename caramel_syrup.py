6e9763ecf9193f50cde9b4fbf62381b3bbaaf7d6
Caleb Sigmon <cjsigmon@email.unc.edu>
2024-04-29 10:53:27 -0400

    fix caramel syrup

--- caramel_syrup.py
+++ caramel_syrup.py
@@ -28,7 +28,7 @@ def move_cup_to_caramel():
     # up, front of syrups tray
     arm.set_servo_angle(angle=[-0.8, -20.5, -13.7, 100.4, 82.2, 32.2], speed=arm_speed,  wait=False, radius=0.0)
     # go in for caramel
-    arm.set_servo_angle(angle=[32.1, 13.9, -43.1, 127.1, 73.1, 25.0], speed=arm_speed,  wait=False, radius=0.0)
+    arm.set_servo_angle(angle=[34.4, 4.5, -36.1, 132.5, 64.9, 18.8], speed=arm_speed,  wait=False, radius=0.0)
     # let go
     arm.set_gripper_position(800, wait=True, speed=5000, auto_enable=True)
 
@@ -52,13 +52,21 @@ def squirt_caramel_grab_cup():
     # unspin
     arm.set_servo_angle(angle=[60.6, 24.9, -113.8, 127.9, 1.1, -44.3], speed=arm_speed, wait=False, radius=0.0)
     arm.set_servo_angle(angle=[50.8, -31.4, -39.3, 138.9, 24.6, 41.1], speed=arm_speed, wait=False, radius=0.0)
-    arm.set_servo_angle(angle=[32.1, 14.1, -42.3, 127.1, 73.0, 25.0], speed=arm_speed, wait=False, radius=0.0)
+    arm.set_servo_angle(angle=[39.9, -1.8, -37.9, 138.9, 55.5, 22.9], speed=arm_speed, wait=False, radius=0.0)
+    arm.set_servo_angle(angle=[38.6, -4.1, -34.4, 138.9, 55.5, 22.9], speed=arm_speed, wait=False, radius=0.0)
+
+    arm.set_position(*[449.4, 534.7, 196.8, -159.2, 81.1, -85], speed=arm_speed, radius=0.0, wait=False)
+
     # grab syrupy cupy
     arm.set_gripper_position(305, wait=True, speed=5000, auto_enable=True)
     
 
 def get_cup_again_caramel():
     # move up/back
+    arm.set_servo_angle(angle=[28, -1.1, -41.6, 110.5, 67.3, 38.3], speed=arm_speed, wait=False, radius=0.0)
+    arm.set_servo_angle(angle=[18.1, -28, -15.9, 110.5, 67.2, 38.3], speed=arm_speed, wait=False, radius=0.0)
+
+
     arm.set_servo_angle(angle=[-0.8, -20.5, -13.7, 100.4, 82.2, 32.2], speed=arm_speed, wait=False, radius=0.0)
     # move down
     arm.set_servo_angle(angle=[-11.1, 4.2, -20.0, 91.9, 82.4, 11.9], speed=arm_speed, wait=False, radius=0.0)
